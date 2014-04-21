99c64d476f14a4aa87a930f62cfe9deb2b9a8002
Justin Kenyon <kenyonj@gmail.com>
2014-04-21 00:23:40 -0400

    fix double quotes and redundant self

--- app/models/url.rb
+++ app/models/url.rb
@@ -11,14 +11,14 @@ class Url < ActiveRecord::Base
   private
 
   def check_if_url_is_valid
-    unless self.original_url.strip.downcase.start_with?("http")
-      errors.add(:url, "must start with http or https.")
+    unless self.original_url.strip.downcase.start_with?('http')
+      errors.add(:url, 'must start with http or https.')
     end
   end
 
   def generate_unique_token
     self.token = SecureRandom.hex(2)
-    while Url.exists?(token: self.token)
+    while Url.exists?(token: token)
       self.token = SecureRandom.hex(2)
     end
   end
