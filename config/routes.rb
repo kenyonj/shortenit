33ce0a41732b35e0938dcf8c7705fbdb7b600f33
Justin Kenyon <kenyonj@gmail.com>
2014-04-21 00:42:59 -0400

    overide clearance routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Shortenit::Application.routes.draw do
   root 'urls#new'
+  resources :passwords,
+    controller: 'clearance/passwords',
+    only: [:create, :new]
+
+  resource :session,
+    controller: 'clearance/sessions',
+    only: [:create]
+
+  resources :users,
+    controller: 'clearance/users',
+    only: Clearance.configuration.user_actions do
+      resource :password,
+        controller: 'clearance/passwords',
+        only: [:create, :edit, :update]
+  end
+
+  get '/sign_in' => 'clearance/sessions#new', as: ''
+  delete '/sign_out' => 'clearance/sessions#destroy', as: ''
+
+  if Clearance.configuration.allow_sign_up?
+    get '/sign_up' => 'clearance/users#new', as: ''
+  end
 
   resources :urls, only: [:new, :create, :show]
 
