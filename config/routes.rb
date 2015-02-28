031f4ad651c55a934d4c005667d6b31ffb7e9694
Justin Kenyon <kenyonj@gmail.com>
2015-02-28 10:17:45 -0500

    Updates IP blocking technique

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Shortenit::Application.routes.draw do
-  root 'urls#new'
+  constraints ip: IpsRouting.new do
+    root 'urls#new'
 
-  resources :urls, only: [:new, :create, :show]
+    resources :urls, only: [:new, :create, :show]
 
-  resources :redirects, only: [:show], path: ''
+    resources :redirects, only: [:show], path: ''
 
-  get '/shorten/create' => 'bookmarklets#create'
+    get '/shorten/create' => 'bookmarklets#create'
+  end
+
+  class IpsRouting
+    def initialize
+      @ips = ENV['BANNED_IPS']
+    end
+
+    def matches?(request)
+      return false if !@ips.include?(request.remote_ip)
+      true
+    end
+  end
 end
